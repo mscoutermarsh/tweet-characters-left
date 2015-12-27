d4eb1670634969954b7313bfdf0d940cf33993e1
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-12-26 21:17:52 -0800

    Add example for tonic

---
+++ example.js
@@ -0,0 +1,7 @@
+var tweetCharactersLeft = require("tweet-characters-left");
+
+// Twitters most common tweet.
+console.log(tweetCharactersLeft("Bieber! Marry me!"));
+
+// If we also included an image.
+console.log(tweetCharactersLeft("Bieber! Marry me!", true));
