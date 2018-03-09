c8f955c6e3360ee211ac1acc83643acfa0f95bc8
Pablo Nevares <pablo@nevares.com>
2018-03-09 09:53:04 -0700

    Bumping to twitter-text 2.x.x, porting from deprecated method getTweetLength

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const twitterText = require('twitter-text');
 const TWEET_LENGTH = 140;
 
 module.exports = function tweetCharactersLeft(text) {
-  return TWEET_LENGTH - twitterText.getTweetLength(text);
+  return TWEET_LENGTH - twitterText.parseTweet(text).weightedLength;
 }
