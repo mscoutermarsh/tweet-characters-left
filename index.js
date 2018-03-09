fd235d5e6ba15dd327cd9acb8b55475fc4a45cb2
Pablo Nevares <pablo@nevares.com>
2018-03-09 09:53:36 -0700

    Updating module and tests for 280-character limit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const twitterText = require('twitter-text');
 
-const TWEET_LENGTH = 140;
+const TWEET_LENGTH = 280;
 
 module.exports = function tweetCharactersLeft(text) {
   return TWEET_LENGTH - twitterText.parseTweet(text).weightedLength;
